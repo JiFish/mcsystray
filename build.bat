33d8613826871854d5ca12635f74345276b5a62b
JiFish <joe@jifish.co.uk>
2016-06-19 04:41:45 +0100

    Add misc files to windows build script

--- build.bat
+++ build.bat
@@ -1,1 +1,4 @@
-pyinstaller.exe -F --noconsole --icon=redstonelamp.ico mcsystray.pyw
+pyinstaller.exe -F --noconsole --icon=redstonelamp.ico --onefile mcsystray.pyw
+copy /b /y icon_*.png dist\icon_*.png
+copy /b /y config.ini dist\config.ini
+copy /b /y corsair_keylist.txt dist\corsair_keylist.txt
